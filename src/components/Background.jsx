305e089c12b2210174c0e99107b081123d1cc8ee
Abhishek Chaurasiya <abhichek2580@gmail.com>
2023-04-01 14:17:25 +0530

    initial commit

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import sunClearBg from "../assets/clear.jpg";
+import sunbg from "../assets/sunbg.jpg"
 
 const Background = () => {
 
     return (
         <div className='relative w-full h-full'>
-            <img src={sunClearBg} alt="" className='w-full h-[100vh] object-cover ' />
+            <img src={sunbg} alt="" className='w-full h-[100vh] object-cover ' />
         </div>
     )
 }
--- src/components/LeftWeather.jsx
+++ src/components/LeftWeather.jsx
@@ -8,20 +8,22 @@ const LeftWeather = () => {
     return (
         <>
             <div
-                className='text-white absolute top-[10vh] left-[15vw] flex  w-full max-w-[650px] items-center gap-x-20 '>
-                <h1 className=' font-bold text-[45px]'
-                >{temp}
-                    <sup className=' font-bold text-[25px]'> 0</sup><span className='text-[40px]'>C</span>
-                </h1>
+                className='text-white absolute bottom-[15vh] left-[15vw] flex  w-full max-w-[650px] items-center gap-x-20 '>
+                <div className='flex'>
+                    <h1 className='font-bold text-[45px]'>{temp}  </h1>
+                    <p> <sup className='ml-[4px] font-bold text-[25px]'> 0</sup><span className='text-[40px]'>C</span>  </p>
+                </div>
                 <div className='flex flex-col items-center'>
                     <p className='font-blod text-[45px]'>{city}</p>
                     <div className='flex items-center gap-x-4'>
-                        <p className='font-blod text-[18px]'>{time}</p>
-                        <p className='font-blod text-[18px]'>{date.toLocaleDateString()}</p>
                     </div>
                 </div>
                 <div>
                     <IconWeather />
+                </div>
+                <div className='flex flex-col items-center gap-y-1 justify-center'>
+                    <p className='font-blod text-[18px]'>{time}</p>
+                    <p className='font-blod text-[18px] px-4'>{date.toLocaleDateString()}</p>
                 </div>
             </div>
         </>
